f996f6d7f974533f120ae6c4a24c74223f16dacf
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-06-03 20:33:02 -0300

    Versão final

--- Aula_01/Ano1.py
+++
@@ -1,5 +0,0 @@
-ano = input("Digite o ano: ") # está sendo inserido como uma string
-ano = int(ano) #passando como um valor inteiro
-#calcular a idade
-idade = 2025 - ano 
-print("Sua idade é: ", idade) #imprimir a idade
